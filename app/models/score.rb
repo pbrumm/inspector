2c84e7ac77bd73c78f843aff08d8f280cee607be
Sam Walton <sam@foofrancine.com>
2014-03-17 11:43:14 -0500

    single item on inspection#show b4 JS

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,10 +1,11 @@
 class Score < ActiveRecord::Base
 	belongs_to :item
 	belongs_to :inspection
-	scope :videos, -> { where(type: 'Video') }
-	scope :voids, -> { where(type: 'Void') }
-	scope :no_sales, -> { where(type: 'NoSale') }
-	scope :general, -> { where(type: 'General') }
+	# scope :videos, -> { where(type: 'Video') }
+	# scope :voids, -> { where(type: 'Void') }
+	# scope :no_sales, -> { where(type: 'NoSale') }
+	# scope :general, -> { where(type: 'General') }
+	scope :totals, -> { where(inspection_id: @inspection.id) }
 end
 
   class << self
