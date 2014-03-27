460ed04cf25c907dc753dac0ccdd4a7b17f4f0be
Pete Brumm <pete@petebrumm.com>
2014-03-26 22:31:13 -0500

    fixing form to make it easier to know which was clicked

--- app/assets/javascripts/inspection.js
+++ app/assets/javascripts/inspection.js
@@ -1,34 +1,30 @@
 $(function() {
-$(".pagination a").click(function(e){e.preventDefault();
-	var tp_id = $(this).attr('href').split('?page=')[1]; 
-	$('form').append("<input type='hidden' name='score[survey_id]' value='"+ tp_id +"'>");
-	$('form').submit();
-})});
+  
+// $(".pagination a").click(function(e){e.preventDefault();
+//   var tp_id = $(this).attr('href').split('?page=')[1]; 
+//   $('form').submit();
+// })
 
-$(function() {
-
-  function showGetResult() {
-  var result = null;
-  $.ajax({
-	url: '/inspections/' + $('div.run-score-data').data("iid") + '.json',
-	dataType: 'json',
-	async: false,
-	success: function(data) {
-		result = data;
-	}
-	});
-return result;
-}
+// function showGetResult() {
+//   var result = null;
+//   $.ajax({
+//     url: '/inspections/' + $('div.run-score-data').data("iid") + '.json',
+//     dataType: 'json',
+//     async: false,
+//     success: function(data) {
+//       result = data;
+//     }
+//   });
+//   return result;
+// }
 
 
-  $(".item_act").click(function(){;
-  	var act_id = parseInt($(this).attr('class').split('act-')[1]);
-  	$('form#edit_item_' + act_id).submit();
-  	$('div.act-' + act_id).css("background-color", "rgb(190, 193, 201)");
-  	
+  $("div.item_act:not(.saved)").click(function(ev){
+    var form = $(ev.currentTarget).parents("form")
+    form.find('div.item_act').addClass("saved")
+    form.submit();
+    
+    return false
   });
-
-
-
 
 });
